ab40faa623b7696877c17795b031ba3580d24f3b
Mohamed Amaan <https://github.com/mhdamaan4321-source/Kartify-.git>
2026-08-16 05:56:21 +0530

    Updated navbar ikman style and user profile fields

--- core/models.py
+++ core/models.py
@@ -31,3 +31,15 @@ class ProductImage(models.Model):
 
     def __str__(self):
         return f"Image for {self.product.title if self.product else 'Product'}"
+
+
+    from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    home_address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
