419f3dde1fb7f0f71a585c85461d52519106a10c
Mohamed Amaan <https://github.com/mhdamaan4321-source/Kartify-.git>
2026-08-16 11:49:08 +0530

    Remove obsolete staticfiles dirs

--- kartify_project/settings.py
+++ kartify_project/settings.py
@@ -101,9 +101,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 
 # Media files
 MEDIA_URL = '/media/'
